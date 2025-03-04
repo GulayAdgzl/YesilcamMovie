a68b72f80ff6d9b9d31efd610514cc27bb2e260b
Gülay ADIGÜZEL <glyadgzl@hotmail.com>
2025-03-04 15:55:45 +0300

    Add DetailActivity class for movie detail display

---
+++ app/src/main/java/com/glyadgzl/yesilcammovie/Activity/DetailActivity.kt
@@ -0,0 +1,14 @@
+package com.glyadgzl.yesilcammovie.Activity
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.glyadgzl.yesilcammovie.R
+
+class DetailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
