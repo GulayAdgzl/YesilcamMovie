2f8574330ea79e0d656d741c20f3d9a76db6bb98
Gülay ADIGÜZEL <glyadgzl@hotmail.com>
2025-03-04 16:19:18 +0300

    Refactor DetailActivity to extend BaseActivity; enhance UI with loading indicators and improved image display

--- app/src/main/java/com/glyadgzl/yesilcammovie/Activity/DetailActivity.kt
+++ app/src/main/java/com/glyadgzl/yesilcammovie/Activity/DetailActivity.kt
@@ -7,7 +7,7 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.glyadgzl.yesilcammovie.R
 
-class DetailActivity : AppCompatActivity() {
+class DetailActivity :BaseActivity() {
     private lateinit var filItem:FilmItemModel
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,6 +26,64 @@ fun DeatilScreen(film:FilItemModel,onBackClick:()->Unit){
     val isLoading=remember{mutableStateOf(false)}
 
     Box(modifier=Modifier.fillMaxSize().
-    background(color=colorResource(id = R.color.background))
-    )
+    background(color=colorResource(id = R.color.blackBackground))
+    ){
+        if(isLoading.value){
+            CircularProgressIndicator(modifier=Modifier.align(Alignment.Center))
+        }else{
+            Column(modifier=Modifier.fillMaxSize().verticalScroll(scrollState)){
+                Box(modifier=Modifier.height(400.dp)){
+                    Image(
+                        contentDescription = null,
+                        painter=painterResource(id = R.drawable.back),
+                        modifier=Modifier.padding(start=16.dp,top=48.dp)
+                        .clickable{onBackClick()}
+                    )
+                    Image(
+                        contentDescription = null,
+                        painter=painterResource(id = R.drawable.fav),
+                        modifier=Modifier.padding(end=16.dp,top=48.dp)
+                        .align(Alignment.TopEnd)
+                    )
+
+                    AsyncImage(
+                        model=film.Poster,
+                        contentDescription = null,
+                        modifier=Modifier.fillMaxSize(),
+                        contentScale=ContentScale.Crop,
+                        alpha=0.1f
+                    )
+                    AsyncImage(
+                        model=film.Poster,
+                        contentDescription = null,
+                        modifier=Modifier.
+                        size(210.dp,300.dp).clip(RoundedCornerShape(30.dp))
+                        .align(Alignment.BottomCenter),
+                        contentScale=ContentScale.Crop
+                   
+                    )
+                    Box(modifier=Modifier
+                    .height(100.dp).fillMaxWidth()
+                    .align(Alignment.BottomCenter)
+                    .background(
+                        brush=Brush.linearGradient(
+                            color= listOf(
+                                colorResource(R.color.black2),
+                                colorResource(R.color.black1)
+                            ),
+                            start=Offset(0f,0f),
+                            end=Offset(0f,Float.POSITIVE_INFINITY)
+
+                        )
+                    )
+                    
+                    
+                    
+                    )
+
+
+                }
+            }
+        }
+    }
 }
